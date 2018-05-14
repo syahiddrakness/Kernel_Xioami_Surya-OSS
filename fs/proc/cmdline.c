6a557de1ab7f34618909eb7076755249fecbc7d7
dianlujitao <dianlujitao@lineageos.org>
2018-05-14 22:55:24 +0800

    fs/proc: Override reboot boot mode with normal
     * AOSP framework/base commit dca36e4e loads persistent OEM specific USB
       functions with respect to the current boot mode if it's neither
       "normal" nor "unknown". In this case if the corresponding USB
       function is not set, it'll be set to "none".
     * However, while most bootloaders report boot mode as "normal" after
       reboot, OnePlus 3's bootloader reports "reboot". The system will
       treat it as OEM specific boot mode and try to load specific USB
       functions. As the functions are not set, sys.usb.config will be set
       to none.
     * To address the problem, hack cmdline to override "reboot" boot mode
       with "normal", so userspace won't confuse.
    
    Change-Id: I8925d3b6e102902e0e31fda25ffe568fcd37185a

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -56,6 +56,12 @@ static int __init proc_cmdline_init(void)
 	proc_command_line_init();
 #endif
 
+	char *offset_addr;
+
+	offset_addr = strstr(saved_command_line, "androidboot.mode=reboot");
+	if (offset_addr != NULL)
+		strncpy(offset_addr + 17, "normal", 6);
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
