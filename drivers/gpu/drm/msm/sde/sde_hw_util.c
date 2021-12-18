4f0219548847a188c820e4e858705f8835a8ea95
Cosmin Tanislav <demonsingur@gmail.com>
2021-12-18 13:32:04 +0100

    display: sde: do not pass around offset string
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_hw_util.c
+++ drivers/gpu/drm/msm/sde/sde_hw_util.c
@@ -76,8 +76,7 @@ typedef void (*scaler_lut_type)(struct sde_hw_blk_reg_map *,
 
 void sde_reg_write(struct sde_hw_blk_reg_map *c,
 		u32 reg_off,
-		u32 val,
-		const char *name)
+		u32 val)
 {
 	/* don't need to mutex protect this */
 	if (c->log_mask & sde_hw_util_log_mask)
