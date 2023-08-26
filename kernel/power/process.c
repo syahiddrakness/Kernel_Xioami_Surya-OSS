53eac153e3f99636aae78b34082a845438e523dc
syahiddrakness <syahidselaluspd708@gmail.com>
2023-08-26 15:27:32 +0700

    kernel/power: process: Fix freeze Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -27,7 +27,7 @@
 /*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 40 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
@@ -75,11 +75,11 @@ static int try_to_freeze_tasks(bool user_only)
 		/*
 		 * We need to retry, but first give the freezing tasks some
 		 * time to enter the refrigerator.  Start with an initial
-		 * 1 ms sleep followed by exponential backoff until 8 ms.
+		 * 6 ms sleep followed by exponential backoff until 12 ms.
 		 */
-		usleep_range(sleep_usecs / 1, sleep_usecs);
-		if (sleep_usecs < 8 * USEC_PER_MSEC)
-			sleep_usecs *= 1;
+		usleep_range(sleep_usecs / 6, sleep_usecs);
+		if (sleep_usecs < 12 * USEC_PER_MSEC)
+			sleep_usecs *= 6;
 	}
 
 	end = ktime_get_boottime();
@@ -89,12 +89,12 @@ static int try_to_freeze_tasks(bool user_only)
 	if (wakeup) {
 		pr_cont("\n");
 		pr_err("Freezing of tasks aborted after %d.%03d seconds",
-		       elapsed_msecs / 1000, elapsed_msecs % 1000);
+		       elapsed_msecs / 2000, elapsed_msecs % 2000);
 	} else if (todo) {
 		pr_cont("\n");
 		pr_err("Freezing of tasks failed after %d.%03d seconds"
 		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
-		       elapsed_msecs / 1000, elapsed_msecs % 1000,
+		       elapsed_msecs / 2000, elapsed_msecs % 2000,
 		       todo - wq_busy, wq_busy);
 
 		if (wq_busy)
@@ -108,8 +108,8 @@ static int try_to_freeze_tasks(bool user_only)
 		}
 		read_unlock(&tasklist_lock);
 	} else {
-		pr_cont("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
-			elapsed_msecs % 1000);
+		pr_cont("(elapsed %d.%03d seconds) ", elapsed_msecs / 2000,
+			elapsed_msecs % 2000);
 	}
 
 	return todo ? -EBUSY : 0;
