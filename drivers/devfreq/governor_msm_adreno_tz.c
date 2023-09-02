c703ffcc4ae34488e4f7ef3df4fb0fb7e0f9573b
syahiddrakness <syahidselaluspd708@gmail.com>
2023-09-02 22:28:12 +0700

    drivers/devfreq: governor_msm_adreno_tz: Change levels adrenoboost
    Level: low,medium,high
    
    Force levels andrenoboost to low by default
    
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -60,7 +60,7 @@ static DEFINE_SPINLOCK(suspend_lock);
 #define TAG "msm_adreno_tz: "
 
 #if 1
-static unsigned int adrenoboost = 3;
+static unsigned int adrenoboost = 1;
 #endif
 
 static u64 suspend_time;
@@ -100,8 +100,8 @@ static ssize_t adrenoboost_save(struct device *dev,
 {
 	int input;
 	sscanf(buf, "%d ", &input);
-	if (input < 0 || input > 3) {
-		adrenoboost = 3;
+	if (input < 1 || input > 3) {
+		adrenoboost = 1;
 	} else {
 		adrenoboost = input;
 	}
