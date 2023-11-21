357efa250cdf2f6b068f3c51bd3bf62bebc10bb4
syahiddraakness <syahidselaluspd708@gmail.com>
2023-11-21 16:33:40 +0700

    qcom: Setting PM QOS Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1147,25 +1147,25 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	/* get pm-qos-active-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-active-latency",
 		&device->pwrctrl.pm_qos_active_latency))
-		device->pwrctrl.pm_qos_active_latency = 80;
+		device->pwrctrl.pm_qos_active_latency = 15;
 
 	/* get pm-qos-cpu-mask-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,l2pc-cpu-mask-latency",
 		&device->pwrctrl.pm_qos_cpu_mask_latency))
-		device->pwrctrl.pm_qos_cpu_mask_latency = 80;
+		device->pwrctrl.pm_qos_cpu_mask_latency = 15;
 
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
-		device->pwrctrl.pm_qos_wakeup_latency = 80;
+		device->pwrctrl.pm_qos_wakeup_latency = 15;
 
 	/* override these */
-	device->pwrctrl.pm_qos_active_latency = 80;
-	device->pwrctrl.pm_qos_cpu_mask_latency = 80;
-	device->pwrctrl.pm_qos_wakeup_latency = 80;
+	device->pwrctrl.pm_qos_active_latency = 15;
+	device->pwrctrl.pm_qos_cpu_mask_latency = 15;
+	device->pwrctrl.pm_qos_wakeup_latency = 15;
 
 	if (of_property_read_u32(node, "qcom,idle-timeout", &timeout))
-		timeout = 80;
+		timeout = 30;
 
 	device->pwrctrl.interval_timeout = msecs_to_jiffies(timeout);
 
