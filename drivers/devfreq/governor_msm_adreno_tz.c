fd1c9a72d34bfc495963d1716d0049103e61516f
syahiddraakness <syahidselaluspd708@gmail.com>
2023-11-21 16:27:26 +0700

    drivers/devfreq: governor_msm_adreno_tz: Change levels adrenoboost Force levels andrenoboost to OFF by default
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -60,7 +60,7 @@ static DEFINE_SPINLOCK(suspend_lock);
 #define TAG "msm_adreno_tz: "
 
 #if 1
-static unsigned int adrenoboost = 3;
+static unsigned int adrenoboost = 0;
 #endif
 
 static u64 suspend_time;
@@ -101,7 +101,7 @@ static ssize_t adrenoboost_save(struct device *dev,
 	int input;
 	sscanf(buf, "%d ", &input);
 	if (input < 0 || input > 3) {
-		adrenoboost = 3;
+		adrenoboost = 0;
 	} else {
 		adrenoboost = input;
 	}
