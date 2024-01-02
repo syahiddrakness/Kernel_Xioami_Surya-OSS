b58554e6a8e929b8446857901917a40b5bc9159c
syahiddraakness <syahidselaluspd708@gmail.com>
2024-01-03 05:31:29 +0700

    Revert " UPSTREAM: fuse: fix matching of FUSE_DEV_IOC_CLONE command " Revert Commit 2f35e69
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -2288,8 +2288,11 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 	int oldfd;
 	struct fuse_dev *fud = NULL;
 
-	switch (cmd) {
-	case FUSE_DEV_IOC_CLONE:
+	if (_IOC_TYPE(cmd) != FUSE_DEV_IOC_MAGIC)
+		return -EINVAL;
+
+	switch (_IOC_NR(cmd)) {
+	case _IOC_NR(FUSE_DEV_IOC_CLONE):
 		res = -EFAULT;
 		if (!get_user(oldfd, (__u32 __user *)arg)) {
 			struct file *old = fget(oldfd);
@@ -2314,7 +2317,7 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 			}
 		}
 		break;
-	case FUSE_DEV_IOC_PASSTHROUGH_OPEN:
+	case _IOC_NR(FUSE_DEV_IOC_PASSTHROUGH_OPEN):
 		res = -EFAULT;
 		if (!get_user(oldfd, (__u32 __user *)arg)) {
 			res = -EINVAL;
