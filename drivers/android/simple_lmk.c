e3c87895a31ddc56e29d0fd694dac96cc67eafed
syahiddraakness <syahidselaluspd708@gmail.com>
2024-02-17 23:25:45 +0700

    Revert " drivers/android: simple_lmk: Add LITTE CPU CLUSTER To Reclaim Kthread " Revert Commit 4f42620 Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -368,7 +368,7 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 	struct task_struct *thread;
 
 	if (!atomic_cmpxchg(&init_done, 0, 1)) {
-		thread = kthread_run_perf_critical(cpu_perf_mask, cpu_all_mask, cpu_lp_mask, cpu_online_mask, 
+		thread = kthread_run_perf_critical(cpu_perf_mask, 
 						   simple_lmk_reclaim_thread,
 						   NULL, "simple_lmkd");
 		BUG_ON(IS_ERR(thread));
