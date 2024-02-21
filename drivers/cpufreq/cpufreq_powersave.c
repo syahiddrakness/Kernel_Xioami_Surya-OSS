19c3800174e6f4d3fa3e4014dd2c1e21fd4481eb
syahiddraakness <syahidselaluspd708@gmail.com>
2024-02-21 08:29:56 +0700

    drivers/cpufreq: cpufreq_powersave: Add  KProfiles Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/cpufreq/cpufreq_powersave.c
+++ drivers/cpufreq/cpufreq_powersave.c
@@ -16,10 +16,37 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+
+#ifdef CONFIG_KPROFILES
+extern int kp_active_mode(void);
+#endif
+
 static void cpufreq_gov_powersave_limits(struct cpufreq_policy *policy)
 {
+
+#ifdef CONFIG_KPROFILES
+		switch (kp_active_mode()) {
+		case 0:
+		case 1:
+			pr_debug("setting to %u kHz\n", policy->min);
+			__cpufreq_driver_target(policy, policy->min, CPUFREQ_RELATION_L);
+			break;
+		case 2:
+			pr_debug("setting to %u kHz\n", policy->max);
+			__cpufreq_driver_target(policy, policy->max, CPUFREQ_RELATION_L);
+			break;
+		case 3:
+			pr_debug("setting to %u kHz\n", policy->max);
+			__cpufreq_driver_target(policy, policy->max, CPUFREQ_RELATION_L);
+			break;
+		default:
+			break;
+		}
+#else
 	pr_debug("setting to %u kHz\n", policy->min);
 	__cpufreq_driver_target(policy, policy->min, CPUFREQ_RELATION_L);
+
+#endif
 }
 
 static struct cpufreq_governor cpufreq_gov_powersave = {
