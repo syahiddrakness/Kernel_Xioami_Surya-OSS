26a05c982ba4bd46d13a959666503e97a3808407
syahiddraakness <syahidselaluspd708@gmail.com>
2024-02-25 09:45:41 +0700

    Revert "block: optimize __blk_segment_map_sg() for single-page bvec" Revert Commit 1a37040
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -482,7 +482,7 @@ static int blk_phys_contig_segment(struct request_queue *q, struct bio *bio,
 	return biovec_phys_mergeable(q, &end_bv, &nxt_bv);
 }
 
-static inline struct scatterlist *blk_next_sg(struct scatterlist **sg,
+static struct scatterlist *blk_next_sg(struct scatterlist **sg,
 		struct scatterlist *sglist)
 {
 	if (!*sg)
@@ -547,12 +547,7 @@ __blk_segment_map_sg(struct request_queue *q, struct bio_vec *bvec,
 		(*sg)->length += nbytes;
 	} else {
 new_segment:
-		if (bvec->bv_offset + bvec->bv_len <= PAGE_SIZE) {
-			*sg = blk_next_sg(sg, sglist);
-			sg_set_page(*sg, bvec->bv_page, nbytes, bvec->bv_offset);
-			(*nsegs) += 1;
-		} else
-			(*nsegs) += blk_bvec_map_sg(q, bvec, sglist, sg);
+		(*nsegs) += blk_bvec_map_sg(q, bvec, sglist, sg);
 	}
 	*bvprv = *bvec;
 }
