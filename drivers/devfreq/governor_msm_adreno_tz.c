d0fcfb004d4da3acdb828b3964c98d03c48e0de7
syahiddraakness <syahidselaluspd708@gmail.com>
2024-03-11 18:26:32 +0700

    qcom: Change Idle Timer Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -29,21 +29,21 @@ static DEFINE_SPINLOCK(tz_lock);
 static DEFINE_SPINLOCK(sample_lock);
 static DEFINE_SPINLOCK(suspend_lock);
 /*
- * FLOOR is 20msec to capture up to 3 re-draws
+ * FLOOR is 10msec to capture up to 3 re-draws
  * per frame for 60fps content.
  */
-#define FLOOR		        20000
+#define FLOOR		        1000
 /*
- * MIN_BUSY is 10 msec for the sample to be sent
+ * MIN_BUSY is 1 msec for the sample to be sent
  */
-#define MIN_BUSY		10000
+#define MIN_BUSY		1000
 #define MAX_TZ_VERSION		0
 
 /*
- * CEILING is 120msec, larger than any standard
+ * CEILING is 10msec, larger than any standard
  * frame length, but less than the idle timer.
  */
-#define CEILING			120000
+#define CEILING			10000
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
 #define TZ_INIT_ID		0x6
