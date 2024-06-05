eb306fc3dbbad7d2b1f4f9464a26e0f40d07ba55
syahiddrakness <syahidselaluspd708@gmail.com>
2024-06-05 14:28:28 +0700

    AnyKernel: Updates Scripts AnyKernel 05062024
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- AnyKernel/anykernel.sh
+++ AnyKernel/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel Kernel Caba Rawit by syahid
+kernel.string=Kernel Kernel Falcon by syahid
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
