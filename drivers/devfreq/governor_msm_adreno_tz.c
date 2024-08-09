b4d25f0a1e8453f06aa2780c60ddac41c840fca2
syahiddrakness <syahidselaluspd708@gmail.com>
2024-08-09 20:43:59 +0700

    surya: Change Idler
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -29,21 +29,21 @@ static DEFINE_SPINLOCK(tz_lock);
 static DEFINE_SPINLOCK(sample_lock);
 static DEFINE_SPINLOCK(suspend_lock);
 /*
- * FLOOR is 20 msec to capture up to 3 re-draws
+ * FLOOR is 0.1 msec to capture up to 3 re-draws
  * per frame for 60fps content.
  */
-#define FLOOR		        20000
+#define FLOOR		        100
 /*
- * MIN_BUSY is 20 msec for the sample to be sent
+ * MIN_BUSY is 0.1 msec for the sample to be sent
  */
-#define MIN_BUSY		20000
+#define MIN_BUSY		100
 #define MAX_TZ_VERSION		0
 
 /*
- * CEILING is 120 msec, larger than any standard
+ * CEILING is 60 msec, larger than any standard
  * frame length, but less than the idle timer.
  */
-#define CEILING			120000
+#define CEILING			60000
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
 #define TZ_INIT_ID		0x6
