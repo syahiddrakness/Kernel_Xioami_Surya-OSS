0e44656fa13f018ade570f8bfab8fedc85c4c231
syahiddrakness <syahidselaluspd708@gmail.com>
2024-08-16 16:22:04 +0700

    drivers/gpu/msm: adreno: set pm_qos_wakeup_latency
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1156,7 +1156,7 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
-		device->pwrctrl.pm_qos_wakeup_latency = ;
+		device->pwrctrl.pm_qos_wakeup_latency = 60;
 
 	/* override these */
 	device->pwrctrl.pm_qos_active_latency = 60;
