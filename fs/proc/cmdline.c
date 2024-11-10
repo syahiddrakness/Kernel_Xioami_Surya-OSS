46543c51c0925b901cfb158c2ba55b9ee7a43095
syahiddrakness <syahidselaluspd708@gmail.com>
2024-11-10 21:05:29 +0700

    fs/proc: cmdline: Add COUNT & TAGS For SafetyNet
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -75,6 +75,8 @@ static void patch_safetynet_flags(char *cmd)
 	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
 	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
 	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
+	patch_flag(cmd, "androidboot.count=", "100");
+	patch_flag(cmd, "androidboot.tags=", "release-keys");
 }
 
 static int __init proc_cmdline_init(void)
