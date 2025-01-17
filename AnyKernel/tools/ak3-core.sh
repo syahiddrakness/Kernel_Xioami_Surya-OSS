3a5738688eefbf0900c015e4039315c7cc34b32c
syahiddrakness <syahidselaluspd708@gmail.com>
2025-01-17 19:53:06 +0700

    AnyKernel: Updates Scripts AnyKernel 17012025
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- AnyKernel/tools/ak3-core.sh
+++ AnyKernel/tools/ak3-core.sh
@@ -327,7 +327,7 @@ flash_boot() {
           magisk_patched=$?;
         fi;
         if [ $((magisk_patched & 3)) -eq 1 ]; then
-          ui_print " " "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+          ui_print " " "Magisk.zip Not Installed....";
           comp=$(magiskboot decompress kernel 2>&1 | grep -vE 'raw|zimage' | sed -n 's;.*\[\(.*\)\];\1;p');
           (magiskboot split $kernel || magiskboot decompress $kernel kernel) 2>/dev/null;
           if [ $? != 0 -a "$comp" ] && $comp --help 2>/dev/null; then
