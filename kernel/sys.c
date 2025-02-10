7516964b0816230e97572221ef9a1891ef4239df
syahiddrakness <syahidselaluspd708@gmail.com>
2025-02-10 16:20:53 +0700

    kernel: Fake Kernel Version v5.10.234 (uname)
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1214,7 +1214,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "netbpfload", 10) &&
 		current->pid != netbpfload_pid) {
 		netbpfload_pid = current->pid;
-		strcpy(tmp.release, "4.19.0");
+		strcpy(tmp.release, "5.10.234");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			current->comm, current->pid, tmp.release);
 	}
