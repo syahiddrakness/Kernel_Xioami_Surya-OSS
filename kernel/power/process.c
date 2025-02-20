aeb85fe002bcb65662b24729bb25c5fbceadd513
syahiddrakness <syahidselaluspd708@gmail.com>
2025-02-20 23:18:35 +0700

    Revert "kernel/power: process: swith To strstr"
    This reverts commit 9864b716ae10c20c42222628dd6561c18fdaa26e.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -197,7 +197,7 @@ void thaw_fingerprintd(void)
 
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
-		if (!strstr(p->comm, "android.hardware.biometrics.fingerprint@2.3-service.oneplus", 13)) {
+		if (!memcmp(p->comm, "android.hardware.biometrics.fingerprint@2.3-service.oneplus", 13)) {
 			__thaw_task(p);
 			break;
 		}
