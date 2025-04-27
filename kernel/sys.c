ba42493e0d8ea84bf2e12cbfbf06f77f9f074428
syahiddrakness <syahidselaluspd708@gmail.com>
2025-04-28 02:57:49 +0700

    kernel : fake uname kernel v5.10.236
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1221,7 +1221,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "netbpfload", 10) &&
 		current->pid != netbpfload_pid) {
 		netbpfload_pid = current->pid;
-		strcpy(tmp.release, "6.6.83");
+		strcpy(tmp.release, "5.10.236");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			current->comm, current->pid, tmp.release);
 	}
