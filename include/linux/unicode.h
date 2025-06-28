6cd8bc6e2eca1b97d3f424429dcf7c5525508e69
syahiddrakness <syahidselaluspd708@gmail.com>
2025-06-28 17:04:48 +0700

    Revert "UPSTREAM: unicode: pass a UNICODE_AGE() tripple to utf8_load"
    This reverts commit 9cb570aaf4dab63a02c308fb21d4ad9ce2954611.
    
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- include/linux/unicode.h
+++ include/linux/unicode.h
@@ -5,29 +5,6 @@
 #include <linux/init.h>
 #include <linux/dcache.h>
 
-#define UNICODE_MAJ_SHIFT		16
-#define UNICODE_MIN_SHIFT		8
-
-#define UNICODE_AGE(MAJ, MIN, REV)			\
-	(((unsigned int)(MAJ) << UNICODE_MAJ_SHIFT) |	\
-	 ((unsigned int)(MIN) << UNICODE_MIN_SHIFT) |	\
-	 ((unsigned int)(REV)))
-
-static inline u8 unicode_major(unsigned int age)
-{
-	return (age >> UNICODE_MAJ_SHIFT) & 0xff;
-}
-
-static inline u8 unicode_minor(unsigned int age)
-{
-	return (age >> UNICODE_MIN_SHIFT) & 0xff;
-}
-
-static inline u8 unicode_rev(unsigned int age)
-{
-	return age & 0xff;
-}
-
 struct unicode_map {
 	unsigned int version;
 };
@@ -52,7 +29,7 @@ int utf8_casefold(const struct unicode_map *um, const struct qstr *str,
 int utf8_casefold_hash(const struct unicode_map *um, const void *salt,
 		       struct qstr *str);
 
-struct unicode_map *utf8_load(unsigned int version);
+struct unicode_map *utf8_load(const char *version);
 void utf8_unload(struct unicode_map *um);
 
 #endif /* _LINUX_UNICODE_H */
