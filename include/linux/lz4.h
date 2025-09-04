271cc7e024fd6e82cee28119528d7d30f22a8abf
syahiddrakness <syahidselaluspd708@gmail.com>
2025-09-04 10:54:08 +0700

    lz4: Set Speed Compress To 20
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -55,24 +55,24 @@
  * Reduced memory usage can improve speed, due to cache effect
  * Default value is 14, for 16KB, which nicely fits into Intel x86 L1 cache
  */
-#define LZ4_MEMORY_USAGE 18
+#define LZ4_MEMORY_USAGE 20
 
 #define LZ4_MAX_INPUT_SIZE	0x7E000000 /* 2 113 929 216 bytes */
 #define LZ4_COMPRESSBOUND(isize)	(\
 	(unsigned int)(isize) > (unsigned int)LZ4_MAX_INPUT_SIZE \
 	? 0 \
-	: (isize) + ((isize)/255) + 18)
+	: (isize) + ((isize)/255) + 20)
 
 #define LZ4_ACCELERATION_DEFAULT 1
 #define LZ4_HASHLOG	 (LZ4_MEMORY_USAGE-2)
 #define LZ4_HASHTABLESIZE (1 << LZ4_MEMORY_USAGE)
 #define LZ4_HASH_SIZE_U32 (1 << LZ4_HASHLOG)
 
-#define LZ4HC_MIN_CLEVEL			18
-#define LZ4HC_DEFAULT_CLEVEL			18
-#define LZ4HC_MAX_CLEVEL			18
+#define LZ4HC_MIN_CLEVEL			20
+#define LZ4HC_DEFAULT_CLEVEL			20
+#define LZ4HC_MAX_CLEVEL			20
 
-#define LZ4HC_DICTIONARY_LOGSIZE 18
+#define LZ4HC_DICTIONARY_LOGSIZE 20
 #define LZ4HC_MAXD (1<<LZ4HC_DICTIONARY_LOGSIZE)
 #define LZ4HC_MAXD_MASK (LZ4HC_MAXD - 1)
 #define LZ4HC_HASH_LOG (LZ4HC_DICTIONARY_LOGSIZE - 1)
@@ -88,7 +88,7 @@
 #define LZ4_STREAMHCSIZE        402192
 #define LZ4_STREAMHCSIZE_SIZET (402192 / sizeof(size_t))
 
-#define LZ4_STREAMDECODESIZE_U64	18
+#define LZ4_STREAMDECODESIZE_U64	20
 #define LZ4_STREAMDECODESIZE		 (LZ4_STREAMDECODESIZE_U64 * \
 	sizeof(unsigned long long))
 
