6b66b25b1c7ecc588a16ac250942d213743d55dc
syahiddrakness <syahidselaluspd708@gmail.com>
2025-12-08 01:59:16 +0000

    drivers: thermal: thermal_core: Change Thermal Profile to 10 (LOCKED)
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -90,13 +90,13 @@ static struct workqueue_struct *thermal_passive_wq;
 #ifdef CONFIG_THERMAL_SUSPEND_RESUME
 static int prev_sconfig = 10;
 static int suspend_sconfig = 10;
-module_param(suspend_sconfig, int, 0644);
+module_param(suspend_sconfig, int, 0444);
 #endif
 
-static int lock_enable = 0;
+static int lock_enable = 10;
 static int lock_sconfig = 10;
-module_param(lock_enable, int, 0644);
-module_param(lock_sconfig, int, 0644);
+module_param(lock_enable, int, 0444);
+module_param(lock_sconfig, int, 0444);
 
 static struct thermal_governor *__find_governor(const char *name)
 {
