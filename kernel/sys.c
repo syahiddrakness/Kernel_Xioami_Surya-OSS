9c11eb13ee84bc26a74a7bd5dc042888fe5d8e7f
syahiddrakness <syahidselaluspd708@gmail.com>
2026-02-27 12:57:23 +0700

    Fix Fake Kernel Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1182,6 +1182,8 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+extern bool legacy_ebpf __read_mostly;
+
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
