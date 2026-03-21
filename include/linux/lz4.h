9e6b364c5ec8257b37044354f85c590074ff9af5
syahiddrakness <syahidselaluspd708@gmail.com>
2026-03-21 22:12:16 +0700

    lz4: Fix Auto Reboot
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -85,8 +85,8 @@
 #define LZ4_STREAMSIZE_U64 ((1 << (LZ4_MEMORY_USAGE - 3)) + 4)
 #define LZ4_STREAMSIZE	(LZ4_STREAMSIZE_U64 * sizeof(unsigned long long))
 
-#define LZ4_STREAMHCSIZE        405443
-#define LZ4_STREAMHCSIZE_SIZET (405443 / sizeof(size_t))
+#define LZ4_STREAMHCSIZE        300000
+#define LZ4_STREAMHCSIZE_SIZET (300000 / sizeof(size_t))
 
 #define LZ4_STREAMDECODESIZE_U64	15
 #define LZ4_STREAMDECODESIZE		 (LZ4_STREAMDECODESIZE_U64 * \
