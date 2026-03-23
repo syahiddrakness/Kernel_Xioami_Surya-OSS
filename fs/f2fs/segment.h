6b7bf782d291c91e29b29c6f9058a3c53944b662
syahiddrakness <syahidselaluspd708@gmail.com>
2026-03-23 15:46:56 +0700

    fs/fs2s: segment: reduce f2fs performance
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -612,11 +612,11 @@ static inline int utilization(struct f2fs_sb_info *sbi)
  * F2FS_IPU_NOCACHE - disable IPU bio cache.
  * F2FS_IPUT_DISABLE - disable IPU. (=default option in LFS mode)
  */
-#define DEF_MIN_IPU_UTIL	70
-#define DEF_MIN_FSYNC_BLOCKS	8
-#define DEF_MIN_HOT_BLOCKS	16
+#define DEF_MIN_IPU_UTIL	35
+#define DEF_MIN_FSYNC_BLOCKS	4
+#define DEF_MIN_HOT_BLOCKS	10
 
-#define SMALL_VOLUME_SEGMENTS	(16 * 512)	/* 16GB */
+#define SMALL_VOLUME_SEGMENTS	(10 * 512)	/* 10GB */
 
 enum {
 	F2FS_IPU_FORCE,
