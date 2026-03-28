fdc71864aafe7b773353448e6342fe7215710feb
syahiddrakness <syahidselaluspd708@gmail.com>
2026-03-28 21:38:09 +0700

    ksu: Add Support KernelSU
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -365,6 +365,12 @@ extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int
 			 int *flags);
 #endif
 
+#ifdef CONFIG_KSU
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
+#endif
+
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -379,6 +385,10 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+
+#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
--- kernel/reboot.c
+++ kernel/reboot.c
@@ -283,7 +283,6 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	struct pid_namespace *pid_ns = task_active_pid_ns(current);
 	char buffer[256];
 	int ret = 0;
-
 	/* We only trust the superuser with rebooting the system. */
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
 		return -EPERM;
