0ca6f296a6bcaa3f4f36b8206cfda07c95697368
syahiddrakness <syahidselaluspd708@gmail.com>
2026-03-29 10:36:53 +0700

    KernelSU-Next: Update KernelSU-Next v1.1.1 (12851)
    Source : https://github.com/KernelSU-Next/KernelSU-Next
    
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/misc/KernelSUNext/throne_tracker.c
+++ drivers/misc/KernelSUNext/throne_tracker.c
@@ -192,7 +192,7 @@ FILLDIR_RETURN_TYPE my_actor(struct dir_context *ctx, const char *name,
 				}
 			}
 
-			bool is_manager = ksu_is_manager_apk(dirpath);
+			bool is_manager = is_manager_apk(dirpath);
 			pr_info("Found new base.apk at path: %s, is_manager: %d\n",
 				dirpath, is_manager);
 			if (is_manager) {
@@ -261,9 +261,8 @@ void search_manager(const char *path, int depth, struct list_head *uid_data)
 	int i, stop = 0;
 	struct list_head data_path_list;
 	INIT_LIST_HEAD(&data_path_list);
-
 	unsigned long data_app_magic = 0;
-
+	
 	// Initialize APK cache list
 	struct apk_path_hash *pos, *n;
 	list_for_each_entry(pos, &apk_path_hash_list, list) {
@@ -352,7 +351,7 @@ static bool is_uid_exist(uid_t uid, char *package, void *data)
 	return exist;
 }
 
-void ksu_track_throne()
+void track_throne()
 {
 	struct file *fp;
 	int tries = 0;
@@ -363,10 +362,11 @@ void ksu_track_throne()
 			if (!IS_ERR(fp)) 
 				break;
 		}
-
+		
 		pr_info("%s: waiting for %s\n", __func__, SYSTEM_PACKAGES_LIST_PATH);
 		msleep(100); // migth as well add a delay
 	};
+	
 	if (IS_ERR(fp)) {
 		pr_err("%s: open " SYSTEM_PACKAGES_LIST_PATH " failed: %ld\n", __func__, PTR_ERR(fp));
 		return;
