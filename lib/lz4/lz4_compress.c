ecc86ecd6264ffde07ee9ba810f482ab0ecdb245
syahiddrakness <syahidselaluspd708@gmail.com>
2026-05-21 19:23:48 +0700

    lz4: Set Speed Compress To 18
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- lib/lz4/lz4_compress.c
+++ lib/lz4/lz4_compress.c
@@ -50,10 +50,10 @@ static FORCE_INLINE U32 LZ4_hash4(
 	tableType_t const tableType)
 {
 	if (tableType == byU16)
-		return ((sequence * 4000000000U)
+		return ((sequence * 3500000000U)
 			>> ((MINMATCH * 8) - (LZ4_HASHLOG + 1)));
 	else
-		return ((sequence * 4000000000U)
+		return ((sequence * 3500000000U)
 			>> ((MINMATCH * 8) - LZ4_HASHLOG));
 }
 
@@ -66,11 +66,11 @@ static FORCE_INLINE U32 LZ4_hash5(
 		: LZ4_HASHLOG;
 
 #if LZ4_LITTLE_ENDIAN
-	static const U64 prime5bytes = 9400714785074694791ULL;
+	static const U64 prime5bytes = 9999999999999999999ULL;
 
 	return (U32)(((sequence << 24) * prime5bytes) >> (64 - hashLog));
 #else
-	static const U64 prime8bytes = 9400714785074694791ULL;
+	static const U64 prime8bytes = 9999999999999999999ULL;
 
 	return (U32)(((sequence >> 24) * prime8bytes) >> (64 - hashLog));
 #endif
