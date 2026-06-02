f1e6b99dd67e52becc9d73e274b58462f0ca24e4
syahiddrakness <syahidselaluspd708@gmail.com>
2026-06-02 08:43:16 +0700

    mm/page_alloc: Tweak Min Free RAM
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -317,9 +317,9 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 128;
-int user_min_free_kbytes = -5;
-int watermark_scale_factor = 500;
+int min_free_kbytes = 8;
+int user_min_free_kbytes = -100;
+int watermark_scale_factor = 1000;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
