ebc5a5095ed60c321df11ff62fbcf15cd5623bf7
syahiddrakness <syahidselaluspd708@gmail.com>
2026-09-18 10:36:33 +0700

    KernelSU-Next: Update KernelSU-Next v3.3.0 (LEGACY)
    Source : https://github.com/KernelSU-Next/KernelSU-Next
    
    Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- drivers/misc/KernelSUNext/selinux/selinux.h
+++ drivers/misc/KernelSUNext/selinux/selinux.h
@@ -1,27 +1,72 @@
 #ifndef __KSU_H_SELINUX
 #define __KSU_H_SELINUX
 
-#include "linux/types.h"
-#include "linux/version.h"
+#include <linux/types.h>
+#include <linux/version.h>
+#include <linux/cred.h>
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)) || defined(KSU_COMPAT_HAS_SELINUX_STATE)
-#define KSU_COMPAT_USE_SELINUX_STATE
+#include "objsec.h"
+#include "security.h" // Samsung SELinux Porting
+
+#ifndef KSU_COMPAT_USE_SELINUX_STATE
+#include "avc.h"
 #endif
 
-void ksu_setup_selinux(const char *);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 18, 0)
+typedef struct task_security_struct taskcred_sec_t;
+#else
+typedef struct cred_security_struct taskcred_sec_t;
+#endif
 
-void ksu_setenforce(bool);
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 10, 0)) &&                         \
+	!defined(KSU_COMPAT_HAS_CURRENT_SID)
+/*
+ * get the subjective security ID of the current task
+ */
+static inline u32 current_sid(void)
+{
+	const struct task_security_struct *tsec = current_security();
 
-bool ksu_getenforce();
+	return tsec->sid;
+}
+#endif
 
-bool ksu_is_ksu_domain();
+#define KERNEL_SU_DOMAIN "ksu"
+#define KERNEL_SU_FILE "ksu_file"
 
-bool ksu_is_zygote(void *cred);
+#define KERNEL_SU_CONTEXT "u:r:" KERNEL_SU_DOMAIN ":s0"
+#define KSU_FILE_CONTEXT "u:object_r:" KERNEL_SU_FILE ":s0"
+#define ZYGOTE_CONTEXT "u:r:zygote:s0"
+#define INIT_CONTEXT "u:r:init:s0"
 
-void ksu_apply_kernelsu_rules();
+void setup_selinux(const char *, struct cred *);
 
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-bool susfs_is_sid_equal(void *sec, u32 sid2);
+void setenforce(bool);
+
+bool getenforce();
+
+void cache_sid(void);
+
+bool is_task_ksu_domain(const struct cred* cred);
+
+bool is_ksu_domain();
+
+bool is_zygote(const struct cred* cred);
+
+bool is_init(const struct cred* cred);
+
+void apply_kernelsu_rules();
+
+int handle_sepolicy(void __user *user_data, u64 data_len);
+
+void setup_ksu_cred();
+
+void escape_to_root_for_adb_root();
+
+extern u32 ksu_file_sid;
+
+#ifdef CONFIG_KSU_SUSFS
+bool susfs_is_sid_equal(const struct cred *cred, u32 sid2);
 u32 susfs_get_sid_from_name(const char *secctx_name);
 u32 susfs_get_current_sid(void);
 void susfs_set_zygote_sid(void);
@@ -30,8 +75,7 @@ void susfs_set_ksu_sid(void);
 bool susfs_is_current_ksu_domain(void);
 void susfs_set_init_sid(void);
 bool susfs_is_current_init_domain(void);
-#endif
-
-u32 ksu_get_devpts_sid();
+void susfs_set_priv_app_sid(void);
+#endif // #ifdef CONFIG_KSU_SUSFS
 
 #endif
